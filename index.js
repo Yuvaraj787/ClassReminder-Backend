f66adf1a2ea286b0afe17088578ec8f01fceae07
YUVARAJ787 <yuvarajv787@gmail.com>
2024-03-29 13:59:48 +0530

    cors added

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import AuthRoutes from "./auth.js"
 import CourseRoutes from "./courses.js"
 import Attendance from "./att.js"
 import Conn from "./dp_config.js";
+import cors from "cors"
 const app = Express()
 const port = 3000
 import axios from "axios";
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.use("/auth", AuthRoutes)
 app.use("/user", CourseRoutes)
